9b30e325cd7e885416ec49a0eca60975ee0731ae
Ubuntu <ubuntu@Lenovo-B470e.(none)>
2015-01-04 22:03:13 +0800

    add bijiao.c

---
+++ bijiao.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+  int x,y,z,t;
+  scanf("%d%d%d",&x,&y,&z);
+  if(x>y)
+  {t=x;x=y;y=t;}
+  if(x>z)
+  {t=z;z=x;x=t;}
+  if(y>z)
+  {t=y;y=z;z=t;}
+  printf("small to big:%d%d%d\n",x,y,z);
+}
